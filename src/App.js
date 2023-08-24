bc91dcbd56c8fb703f71c638a695055df0ea6c58
Iva Petkova <iva1petkova@gmail.com>
2023-08-24 23:33:38 +0300

    create date counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function changeSteps(value) {
+    setStep(value);
+  }
+
+  function changeCount(value) {
+    setCount(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dates">
+      <Counter label="Step" initialNumber={1} setNumber={changeSteps} />
+      <Counter label="Count" initialNumber={0} setNumber={changeCount} />
+      <Message count={step * count} />
+    </div>
+  );
+}
+
+function Message({ count }) {
+  let d = new Date();
+  d.setDate(d.getDate() + count);
+  let message = `Today is ${d.toUTCString()}`;
+
+  const daysStr = Math.abs(count) !== 1 ? "days" : "day";
+  if (count > parseInt("0")) {
+    message = `${Math.abs(count)} ${daysStr} from today is ${d.toUTCString()}`;
+  } else if (count < parseInt("0")) {
+    message = `${Math.abs(count)} ${daysStr} ago was ${d.toUTCString()}`;
+  }
+  return <div>{message}</div>;
+}
+
+function Counter({ label, initialNumber, setNumber }) {
+  console.log("init", initialNumber);
+  const [value, setValue] = useState(initialNumber);
+
+  setNumber(value);
+
+  function decrease() {
+    setValue((v) => v - 1);
+  }
+
+  function increase() {
+    setValue((v) => v + 1);
+  }
+  return (
+    <div className="box">
+      <button className="button" onClick={decrease}>
+        -
+      </button>
+      <span className="label">
+        {label}: {value}
+      </span>
+      <button className="button" onClick={increase}>
+        +
+      </button>
     </div>
   );
 }
